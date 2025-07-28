e2b8fc8ed318492aeaf18be515daac9f8b08e82a
2411cs010579-lang <2411cs010579@mallareddyuniversity.ac.in>
2025-07-28 15:01:09 +0530

    Fix: updated main.jsx and index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-// import CulturistApp from './Culturist.jsx'
-// import App from './Culturist.jsx'
-// import LandingPage from './LandingPage.jsx'
+import { StrictMode } from 'react';        // <-- Add this import
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+// import CulturistApp from './Culturist.jsx';
+// import LandingPage from './LandingPage.jsx';
 
 createRoot(document.getElementById('root')).render(
-<StrictMode>
-<App />
-    {/* <CulturistApp/> */}
-    {/* <LandingPage/> */}
-</StrictMode>,
-)
+  <StrictMode>
+    <App />
+    {/* <CulturistApp /> */}
+    {/* <LandingPage /> */}
+  </StrictMode>
+);
